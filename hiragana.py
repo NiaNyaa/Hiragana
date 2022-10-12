d7d54f3fa007a2f4cef2e2427e32ed9657173596
NiaNyaa <109681800+NiaNyaa@users.noreply.github.com>
2022-10-12 20:44:09 +0700

    Add files via upload

---
+++ hiragana.py
@@ -0,0 +1,274 @@
+import random
+import time
+import os
+
+ekupi = open('//storage//CC4C-55EE//jpgame//exp.txt','r')
+ekspo = ekupi.read()
+ekspi = int(ekspo)
+
+def hgnk():
+  os.system('clear')
+  while True:
+    a = "あいうえおはひふへほなにぬねのかきくけこさしすせそたちつてとまみむめもやゆよらりるれろわをんがぎぐげござじずぜぞだぢづでどばびぶべぼぱぴぷぺぽ"
+    print()
+    data = random.choice(a)
+    print("{",data,"}")
+    jwb = input("\nHuruf Apa Itu: ").lower()
+    if data =="あ" and jwb =="a":
+      print("[Benar]"),exp(1)
+    
+    elif data =="い" and jwb == "i":
+      print("[Benar]"),exp(1)
+
+    elif data =="う" and jwb == "u":
+      print("[Benar]"),exp(1)
+
+    elif data =="え" and jwb == "e":
+      print("[Benar]"),exp(1)
+
+    elif data =="お" and jwb == "o":
+      print("[Benar]"),exp(1)
+
+    elif data =="は" and jwb == "ha":
+      print("[Benar]"),exp(1)
+
+    elif data =="ひ" and jwb == "hi":
+      print("[Benar]"),exp(1)
+
+    elif data =="ふ" and jwb == "fu":
+      print("[Benar]"),exp(1)
+
+    elif data =="へ" and jwb == "he":
+      print("[Benar]"),exp(1)
+
+    elif data =="ほ" and jwb == "ho":
+      print("[Benar]"),exp(1)
+
+    elif data =="な" and jwb == "na":
+      print("[Benar]"),exp(1)
+
+    elif data =="に" and jwb == "ni":
+      print("[Benar]"),exp(1)
+
+    elif data =="ぬ" and jwb == "nu":
+      print("[Benar]"),exp(1)
+
+    elif data =="ね" and jwb == "ne":
+      print("[Benar]"),exp(1)
+
+    elif data =="の" and jwb == "no":
+      print("[Benar]"),exp(1)
+
+    elif data =="か" and jwb == "ka":
+      print("[Benar]"),exp(1)
+
+    elif data =="き" and jwb == "ki":
+      print("[Benar]"),exp(1)
+
+    elif data =="く" and jwb == "ku":
+      print("[Benar]"),exp(1)
+
+    elif data =="け" and jwb == "ke":
+      print("[Benar]"),exp(1)
+
+    elif data =="こ" and jwb == "ko":
+      print("[Benar]"),exp(1)
+      
+    elif data =="さ" and jwb == "sa":
+      print("[Benar]"),exp(1)
+      
+    elif data =="し" and jwb == "shi":
+      print("[Benar]"),exp(1)
+      
+    elif data =="す" and jwb == "su":
+      print("[Benar]"),exp(1)
+      
+    elif data =="せ" and jwb == "se":
+      print("[Benar]"),exp(1)
+      
+    elif data =="そ" and jwb == "so":
+      print("[Benar]"),exp(1)
+      
+    elif data =="た" and jwb == "ta":
+      print("[Benar]"),exp(1)
+      
+    elif data =="ち" and jwb == "chi":
+      print("[Benar]"),exp(1)
+      
+    elif data =="つ" and jwb == "tsu":
+      print("[Benar]"),exp(1)
+      
+    elif data =="て" and jwb == "te":
+      print("[Benar]"),exp(1)
+      
+    elif data =="と" and jwb == "to":
+      print("[Benar]"),exp(1)
+      
+    elif data =="ま" and jwb == "ma":
+      print("[Benar]"),exp(1)
+      
+    elif data =="み" and jwb == "mi":
+      print("[Benar]"),exp(1)
+      
+    elif data =="む" and jwb == "mu":
+      print("[Benar]"),exp(1)
+      
+    elif data =="め" and jwb == "me":
+      print("[Benar]"),exp(1)
+      
+    elif data =="も" and jwb == "mo":
+      print("[Benar]"),exp(1)
+      
+    elif data =="や" and jwb == "ya":
+      print("[Benar]"),exp(1)
+      
+    elif data =="ゆ" and jwb == "yu":
+      print("[Benar]"),exp(1)
+      
+    elif data =="よ" and jwb == "yo":
+      print("[Benar]"),exp(1)
+      
+    elif data =="ら" and jwb == "ra":
+      print("[Benar]"),exp(1)
+      
+    elif data =="り" and jwb == "ri":
+      print("[Benar]"),exp(1)
+      
+    elif data =="る" and jwb == "ru":
+      print("[Benar]"),exp(1)
+      
+    elif data =="れ" and jwb == "re":
+      print("[Benar]"),exp(1)
+      
+    elif data =="ろ" and jwb == "ro":
+      print("[Benar]"),exp(1)
+      
+    elif data =="わ" and jwb == "wa":
+      print("[Benar]"),exp(1)
+      
+    elif data =="を" and jwb in ("wo","o"):
+      print("[Benar]"),exp(1)
+      
+    elif data =="ん" and jwb == "n":
+      print("[Benar]"),exp(1)
+      
+    elif data =="が" and jwb == "ga":
+      print("[Benar]"),exp(1)
+      
+    elif data =="ぎ" and jwb == "gi":
+      print("[Benar]"),exp(1)
+      
+    elif data =="ぐ" and jwb == "gu":
+      print("[Benar]"),exp(1)
+      
+    elif data =="げ" and jwb == "ge":
+      print("[Benar]"),exp(1)
+      
+    elif data =="ご" and jwb == "go":
+      print("[Benar]"),exp(1)
+      
+    elif data =="ざ" and jwb == "za":
+      print("[Benar]"),exp(1)
+      
+    elif data =="じ" and jwb in ("ji","zi"):
+      print("[Benar]"),exp(1)
+      
+    elif data =="ず" and jwb == "zu":
+      print("[Benar]"),exp(1)
+      
+    elif data =="ぜ" and jwb == "ze":
+      print("[Benar]"),exp(1)
+      
+    elif data =="ぞ" and jwb == "zo":
+      print("[Benar]"),exp(1)
+      
+    elif data =="だ" and jwb == "da":
+      print("[Benar]"),exp(1)
+      
+    elif data =="ぢ" and jwb in ("ji","di"):
+      print("[Benar]"),exp(1)
+      
+    elif data =="づ" and jwb in ("du","zu"):
+      print("[Benar]"),exp(1)
+      
+    elif data =="で" and jwb == "de":
+      print("[Benar]"),exp(1)
+      
+    elif data =="ど" and jwb == "do":
+      print("[Benar]"),exp(1)
+      
+    elif data =="ば" and jwb == "ba":
+      print("[Benar]"),exp(1)
+      
+    elif data =="び" and jwb == "bi":
+      print("[Benar]"),exp(1)
+      
+    elif data =="ぶ" and jwb == "bu":
+      print("[Benar]"),exp(1)
+      
+    elif data =="べ" and jwb == "be":
+      print("[Benar]"),exp(1)
+      
+    elif data =="ぼ" and jwb == "bo":
+      print("[Benar]"),exp(1)
+      
+    elif data =="ぱ" and jwb == "pa":
+      print("[Benar]"),exp(1)
+      
+    elif data =="ぴ" and jwb == "pi":
+      print("[Benar]"),exp(1)
+      
+    elif data =="ぷ" and jwb == "pu":
+      print("[Benar]"),exp(1)
+      
+    elif data =="ぺ" and jwb == "pe":
+      print("[Benar]"),exp(1)
+      
+    elif data =="ぽ" and jwb == "po":
+      print("[Benar]"),exp(1)
+    else:
+      print("[Anda Salah, Coba Lagi]")
+      exp(-1)
+      break
+
+def exp(n):
+  file2 = open('//storage//CC4C-55EE//jpgame//exp.txt','r')
+  text2 = file2.read()
+  a = int(text2)
+  file2.close()
+  file = open('//storage//CC4C-55EE//jpgame//exp.txt','w')
+  text = a + n
+  textint = str(text)
+  file.write(textint)
+  file.close()
+
+if ekspi < 100:
+  print("Level = 0")
+elif ekspi > 100 and ekspi < 200:
+  print("Level = 1")
+elif ekspi > 200 and ekspi < 400:
+  print("Level = 2")  
+elif ekspi > 400 and ekspi < 700:
+  print("Level = 3")
+elif ekspi > 700  and ekspi < 1400:
+  print("Level = 4")  
+elif ekspi > 1400  and ekspi < 1900:
+  print("Level = 5")
+elif ekspi > 1900 and ekspi < 2300:
+  print("Level = 6")
+elif ekspi > 2300 and ekspi < 3000:
+  print("Level = 7")
+elif ekspi > 3000  and ekspi < 4100:
+  print("Level = 8")
+elif ekspi > 4100  and ekspi < 5500:
+  print("Level = 9")
+elif ekspi > 5500:
+  print("Level = 10 (Max)")  
+  
+  
+print("Exp =",ekspi)
+print("\n1. Mulai")
+inp = input("Pilihan: ")
+if inp == "1":
+  hgnk()
+  
